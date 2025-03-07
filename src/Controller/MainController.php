a35928dfc7a1153256b24044974f7103799e2ae2
Julieza <juliezaraper@gmail.com>
2025-03-07 20:46:21 +0800

    Add Twig configuration and implement homepage with project details

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,36 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route(path: '/')]
-    public function homepage(): Response  
+    public function homepage(): Response     // This is the controller method that will be called when the user visits the homepage
     {
-        return new Response(content: "Hello World! Okay na to!");
+        // fetching numeric data, example:
+        $numberOfMyProjects = 1;
+
+        // fetching key-value data, example:
+        $projectName = 'Platform Rebuild';
+        $projectDescription = 'Rebuilding the platform from scratch';
+        $projectDeadline = '2022-12-31';
+        $projectStatus = 'In Progress';
+
+        // create assiciative array/table, example:
+        $myActivitySnapshotTable = [
+            'projectName'=> $projectName,
+            'projectDescription'=> $projectDescription,
+            'projectDeadline'=> $projectDeadline,
+            'projectStatus'=> $projectStatus
+        ];
+
+        // rendering the template
+        return $this->render('main/homepage.html.twig', [
+            'numberOfMyProjects' => $numberOfMyProjects,
+            'myActivitySnapshotTable'=> $myActivitySnapshotTable
+        ]);
     }
 }
